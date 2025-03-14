4f486b1cb02b03c962734e5553acb53ad23ef40b
angelo bessolo <angelobessolo@gmail.com>
2025-03-14 18:16:55 -0500

    Añadir permisos de ejecución para nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,17 +10,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      // type: 'mysql',
+      // host: 'localhost',
+      // port: 3306,
+      // username: 'root',
+      // password: '',
+      // database: 'smart-talent_db',
+      // entities: [ 
+      //   join(__dirname, '**', '*.entity.{ts,js}')
+      // ], 
+      // synchronize: true,
+      // logging: false,   
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'smart-talent_db',
-      entities: [ 
+      host: process.env.DB_HOST || 'db',  // Cambiado a 'db' (nombre del servicio en docker-compose.yml)
+      port: parseInt(process.env.DB_PORT, 10) || 3306, // Usamos el puerto configurado en docker-compose
+      username: process.env.DB_USERNAME || 'root', 
+      password: process.env.DB_PASSWORD || 'root',  // El password debe coincidir con el definido en docker-compose
+      database: process.env.DB_DATABASE || 'smart_talent_db',
+      entities: [
         join(__dirname, '**', '*.entity.{ts,js}')
       ], 
       synchronize: true,
-      logging: false,   
+      logging: false,
     }),   
     AuthModule, 
     TasksModule
