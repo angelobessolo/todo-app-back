8cc9362ad5ac7fce7260d855c46d6e323038a372
angelo bessolo <angelobessolo@gmail.com>
2025-03-14 18:29:50 -0500

    Variables de entorno para railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,11 +22,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       // synchronize: true,
       // logging: false,   
       type: 'mysql',
-      host: process.env.DB_HOST || 'db',  // Cambiado a 'db' (nombre del servicio en docker-compose.yml)
-      port: parseInt(process.env.DB_PORT, 10) || 3306, // Usamos el puerto configurado en docker-compose
-      username: process.env.DB_USERNAME || 'root', 
-      password: process.env.DB_PASSWORD || 'root',  // El password debe coincidir con el definido en docker-compose
-      database: process.env.DB_DATABASE || 'smart_talent_db',
+      host: process.env.DB_HOST, 
+      port: parseInt(process.env.DB_PORT, 10),  
+      username: process.env.DB_USERNAME,  
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE, 
       entities: [
         join(__dirname, '**', '*.entity.{ts,js}')
       ], 
